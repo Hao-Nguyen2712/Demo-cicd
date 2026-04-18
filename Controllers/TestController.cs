8f005aa65f102bebf3e9ec75142555e8ec96ab3b
joon <hao.nguyenchanh2712@gmail.com>
2026-04-18 07:41:47 +0700

    init commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoGiihubActionDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok("hello this is testing from house ");
+        }
+    }
+}
